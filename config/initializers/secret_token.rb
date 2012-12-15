43f26b605391d4fa57d012f50a4fd28c569c43f8
jose miguel <miguel.gamazo@galiclick.com>
2012-12-15 14:05:33 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeJoyeria::Application.config.secret_token = '670bc3633c99ea570d9adb9f71cc301232176cd2987cc05a315d3fa7528dc6b1ad152ced0191c058e2f96dbdd66b39388ebd3bf308b48e00b51c0c327487fb96'
