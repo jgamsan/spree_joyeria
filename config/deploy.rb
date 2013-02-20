368e27bec671fb3ca0efe134509379f942813dc1
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2013-02-20 18:09:27 +0100

    Mas mejoras

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,11 @@
+require "bundler/capistrano"
+set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
 set :application, "joyeria"
 set :domain, "mail.galiclick.com"
 set :user, "galiclick"
 set :repository, "git@github.com:jgamsan/spree_joyeria.git"
 set :scm, :git
 set :keep_releases, 2
-set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 set :use_sudo, false
@@ -35,5 +36,4 @@ after "deploy", "deploy:cleanup"
 #set :whenever_command, "bundle exec whenever"
 #require "whenever/capistrano"
 require "rvm/capistrano"
-require "bundler/capistrano"
 require 'capistrano-unicorn'
