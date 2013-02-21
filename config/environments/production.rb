b8d799455bba2ec16b84463c70bc603971de8e94
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2013-02-21 07:40:00 +0100

    Mas mejoras

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ SpreeJoyeria::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( favicon.ico initial/all.css session.css )
+  config.assets.precompile += %w( favicon.ico initial/all.css session.css store/jquery.roundabout.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
